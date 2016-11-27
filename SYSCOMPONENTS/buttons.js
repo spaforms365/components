99e879433bb7b401a16c4a7b67f7bd34df689a3b
nmukhin2251 <nick.mukhin@gmail.com>
2016-11-27 00:35:53 -0600

    rel 1030

--- SYSCOMPONENTS/buttons.js
+++ SYSCOMPONENTS/buttons.js
@@ -6,27 +6,44 @@ define(['text!./buttons.html'], function( htmlString) {
 		var self = this;
 		self.params = params;
 //debugger;	
+		this.formID = ko.observable("").extend({ listItem: "mwp_FormID" });
+		this.formType = ko.observable("").extend({ listItem: "mwp_FormType" });
+		this.formState = ko.observable("Draft").extend({ listItem: "mwp_FormState" });
 //debugger;
+		this.runtime = ko.observable().extend({form: "runtime"});
+		this.readonly = ko.observable().extend({form: "readonly"});
+		this.designmode = ko.observable().extend({form: "designmode"});
+		if( this.designmode()) {
+			console.log("ensure columns");
+			this.runtime()._formEnsureColumns( this.schema["ListColumns"]).done( function() {
+				console.log("columns ensured");
+			});
+		}
+
 		self.form = ko.observable();//params.form);
 		// SETTINGS
+		self.hideUI = (params.hidden) ? params.hidden : false;
 		self.enableSaveUpdateDeleteButtonOnForms = (params.save !== undefined) ? params.save : false; //self.form()._formSettings
 		self.enableCancelButtonOnSubmittedOrProcessingForms = (params.cancel !== undefined) ? params.cancel : true;
 		self.enableDeleteButtonOnApprovedForms = (params.deletesubmitted != undefined) ? params.deletesubmitted : true;
 		
 		//self._formEditingForm = ko.observable(false);
 		//self._formDesigningForm = ko.observable(false);
-
+		self.enableUI = ko.pureComputed(function() {
+			if( self.designmode()) return true;
+			return (self.hideUI) ? false : true;
+		});
 		
         // SUBMIT
         self._formButtonSubmitClick = function () {
-            self.form().mwp_FormState(self.form()._formStates.SUBMITTED);
-            self.form()._formModerationStatus(2); //SPModerationStatusType.Pending; -- will be applied only for Moderation Mode = DRAFT
-            self.form()._formSave();
+            self.formState(self.form()._formStates.SUBMITTED);
+            self.runtime()._formModerationStatus(2); //SPModerationStatusType.Pending; -- will be applied only for Moderation Mode = DRAFT
+            self.runtime()._formSave();
         };
         self._formButtonSubmitEnabled = ko.pureComputed(function () {
-			if( self.form()) {
-				//var b = (self.form().mwp_FormState() == self.form()._formStates.DRAFT);
-				var b = (self.form()._formReadOnly() == false);
+			if( self.runtime()) {
+				//var b = (self.formState() == self.runtime()._formStates.DRAFT);
+				var b = (self.readonly() == false);
 				return (b.toString().toLowerCase() == 'true') ? true : false;
 			}
 			return false;
@@ -38,24 +55,25 @@ define(['text!./buttons.html'], function( htmlString) {
 			return self._formButtonSubmitEnabled();
 		};
 		self.actionFormsCommandSubmit = function() {
-			self.form()._closeOnButtonClick = true;
+			self.runtime()._closeOnButtonClick = true;
 			self._formButtonSubmitClick();
 		};
         // CANCEL
         self._formButtonCancelClick = function () {
-			if( self.form()) {
-				self.form().mwp_FormState(self.form()._formStates.CANCELLED);
-				if (self.form()._formModerationStatus() == 2) //SPModerationStatusType.Pending
-					self.form()._formModerationStatus(3); //SPModerationStatusType.Draft; -- will be applied only for Moderation Mode = DRAFT
-				self.form()._formSave();
+			if( self.runtime()) {
+				self.formState(self.runtime()._formStates.CANCELLED);
+				if (self.runtime()._formModerationStatus() == 2) //SPModerationStatusType.Pending
+					self.runtime()._formModerationStatus(3); //SPModerationStatusType.Draft; -- will be applied only for Moderation Mode = DRAFT
+				self.runtime()._formSave();
 			}
 			return false;
         };
         self._formButtonCancelEnabled = ko.computed(function () {
-			if( self.form()) {
-				var b = (((self.form().mwp_FormState() != self.form()._formStates.DRAFT) && (self.form().mwp_FormState() != self.form()._formStates.CANCELLED)) // cancel enabled always, except Draft or Cancelled
-						//&& (self.form()._formReadOnly() == false) // read only form can't have cancel button
-						&& (self.form()._formCanEdit() == true) && (self.form()._formEditMode() == true)
+//debugger;			
+			if( self.runtime()) {
+				var b = (((self.formState() != self.runtime()._formStates.DRAFT) && (self.formState() != self.runtime()._formStates.CANCELLED)) // cancel enabled always, except Draft or Cancelled
+						//&& (self.readonly() == false) // read only form can't have cancel button
+						&& (self.runtime()._formCanEdit() == true) && (self.runtime()._formEditMode() == true)
 						&& self.enableCancelButtonOnSubmittedOrProcessingForms); // cancel enabled via options
 				return (b.toString().toLowerCase() == 'true') ? true : false;
 			}
@@ -68,32 +86,32 @@ define(['text!./buttons.html'], function( htmlString) {
 			return self._formButtonCancelEnabled();
 		};
 		self.actionFormsCommandCancel = function() {
-			self.form()._closeOnButtonClick = true;
+			self.runtime()._closeOnButtonClick = true;
 			self._formButtonCancelClick();
 		};
         // DELETE
         self._formButtonDeleteClick = function () {
-            self.form()._formDelete();
+            self.runtime()._formDelete();
         };
 		self._formApproved = function () {
-			if (self.form()._formEnableModeration()) return (self.form()._formModerationStatus() == 0);
+			if (self.runtime()._formEnableModeration()) return (self.runtime()._formModerationStatus() == 0);
 			return true;
 		};
         self._formButtonDeleteEnabled = ko.pureComputed(function () {
-			if( self.form()) {
+			if( self.runtime()) {
 				var moderation;
-				if (self.form()._formModerationMode() == self.form()._formModerationModeLevel.DISABLED) {
-					moderation = (!self._formApproved()) || ((self._formApproved() && ( self.enableDeleteButtonOnApprovedForms || ((self.form().mwp_FormState() == self.form()._formStates.DRAFT) && self.enableSaveUpdateDeleteButtonOnForms) )));
+				if (self.runtime()._formModerationMode() == self.runtime()._formModerationModeLevel.DISABLED) {
+					moderation = (!self._formApproved()) || ((self._formApproved() && ( self.enableDeleteButtonOnApprovedForms || ((self.formState() == self.runtime()._formStates.DRAFT) && self.enableSaveUpdateDeleteButtonOnForms) )));
 				}
 				else {
-					moderation = ((!self._formApproved() || !self.form()._formEnableModeration()) || self.enableDeleteButtonOnApprovedForms);// if moderation is enabled -> then any moderation state enables delete, except APPROVED
+					moderation = ((!self._formApproved() || !self.runtime()._formEnableModeration()) || self.enableDeleteButtonOnApprovedForms);// if moderation is enabled -> then any moderation state enables delete, except APPROVED
 				}
-				var b_draft = ((self.form().mwp_FormState() == self.form()._formStates.DRAFT) && (self.enableSaveUpdateDeleteButtonOnForms));
-				var b_submitted = ((self.form().mwp_FormState() != self.form()._formStates.DRAFT) && (!self._formButtonCancelEnabled()) && self.enableDeleteButtonOnApprovedForms);
+				var b_draft = ((self.formState() == self.runtime()._formStates.DRAFT) && (self.enableSaveUpdateDeleteButtonOnForms));
+				var b_submitted = ((self.formState() != self.runtime()._formStates.DRAFT) && (!self._formButtonCancelEnabled()) && self.enableDeleteButtonOnApprovedForms);
 //debugger;				
-				var b = (!self.form()._formNew() // the new, not created yet form, can't have delete button
-				    //&& (self.form()._formReadOnly() == false) // read only form can't have delete button
-					&& (self.form()._formCanEdit() == true) && (self.form()._formEditMode() == true)
+				var b = (!self.runtime()._formNew() // the new, not created yet form, can't have delete button
+				    //&& (self.readonly() == false) // read only form can't have delete button
+					&& (self.runtime()._formCanEdit() == true) && (self.runtime()._formEditMode() == true)
 					&& moderation && ( b_draft || b_submitted));
 				return (b.toString().toLowerCase() == 'true') ? true : false;
 			}
@@ -110,15 +128,15 @@ define(['text!./buttons.html'], function( htmlString) {
 		};
         // SAVE
         self._formButtonSaveClick = function () {
-            self.form().mwp_FormState(self.form()._formStates.DRAFT); // Saved form is always in Draft state
-            self.form()._formModerationStatus(3);//SPModerationStatusType.Draft; -- will be applied only for Moderation Mode = DRAFT
-            self.form()._formSave();
+            self.formState(self.runtime()._formStates.DRAFT); // Saved form is always in Draft state
+            self.runtime()._formModerationStatus(3);//SPModerationStatusType.Draft; -- will be applied only for Moderation Mode = DRAFT
+            self.runtime()._formSave();
         };
         self._formButtonSaveEnabled = ko.pureComputed(function () {
-			if( self.form()) {
-				var b = ((self.form().mwp_FormState() == self.form()._formStates.DRAFT) // Form can be saved only while in Draft state 
-					 && (self.form()._formReadOnly() == false) // read only form can't have save button
-					 && ((self.form()._formModerationMode() == self.form()._formModerationModeLevel.DRAFT) || (self.enableSaveUpdateDeleteButtonOnForms)));
+			if( self.runtime()) {
+				var b = ((self.formState() == self.runtime()._formStates.DRAFT) // Form can be saved only while in Draft state 
+					 && (self.readonly() == false) // read only form can't have save button
+					 && ((self.runtime()._formModerationMode() == self.runtime()._formModerationModeLevel.DRAFT) || (self.enableSaveUpdateDeleteButtonOnForms)));
 				return (b.toString().toLowerCase() == 'true') ? true : false;
 			}
 			return false;
@@ -130,20 +148,17 @@ define(['text!./buttons.html'], function( htmlString) {
 			return self._formButtonSaveEnabled();
 		};
 		self.actionFormsCommandSave = function() {
-			self.form()._closeOnButtonClick = true;
+			self.runtime()._closeOnButtonClick = true;
 			self._formButtonSaveClick();
 		};
 
         // CLOSE
         self._formButtonCloseClick = function () {
-            self.form()._formRedirectToList();
+            self.runtime()._formRedirectToList();
         }
         self._formButtonCloseEnabled = ko.pureComputed(function () {
             return true;
         });
-		self._formLoadForm = function() {
-			//self.form()._formInitialize();
-		};
 		/**
 		 * CLOSE FORM BUTTON
 		 */
@@ -157,33 +172,31 @@ define(['text!./buttons.html'], function( htmlString) {
 		// Proxy method:  delete list item
 		self.Init = function() {
 						
-			self.form()._formModerationMode((self.params.moderation != undefined) ? self.params.moderation : "Disabled");
-			self.form()._formUniqueIDMethod((self.params.counter != undefined) ? self.params.counter : "Counter" );//this._formSettings.UniqueIDMethod);
-			
-			self._formLoadForm();
+			self.runtime()._formModerationMode((self.params.moderation != undefined) ? self.params.moderation : "Disabled");
+			self.runtime()._formUniqueIDMethod((self.params.counter != undefined) ? self.params.counter : "Counter" );//this._formSettings.UniqueIDMethod);
 		};
 		/**
 		 * EDIT BUTTON
 		 */
 		self.actionFormsCommandEdit = function(enable) {
 			//self._formEditingForm(enable);
-			self.form()._formEditMode(enable);
+			self.runtime()._formEditMode(enable);
 			RefreshCommandUI();
 			return true;
 		};
 		self.queryFormsCommandEdit = function() {
-			return self.form()._formEditMode();
+			return self.runtime()._formEditMode();
 		};
 		/**
 		 * DESIGN BUTTON
 		 */
 		self.actionFormsCommandDesign = function(enable) {
-			self.form()._formDesignMode(enable);
+			self.runtime()._formDesignMode(enable);
 			RefreshCommandUI();
 			return true;
 		};
 		self.queryFormsCommandDesign = function() {
-			return self.form()._formDesignMode();
+			return self.runtime()._formDesignMode();
 		};
 
 	}
@@ -191,11 +204,17 @@ define(['text!./buttons.html'], function( htmlString) {
     //componentbuttons.prototype.doSomething = function() { ... };
 	buttons.prototype.schema = {
 		"Params": {
+			"hidden": false,
 			"save": true,
 			"cancel": true,
 			"deletesubmitted": true,
 			"moderation": "Draft",
 			"uniqueid": "Counter"
+		},
+		"ListColumns" : {
+			"mwp_FormID": '<Field ID="{3c53b379-6256-4ab5-bc14-8fa61360e526}" Type="Text" Name="mwp_FormID" Required="FALSE" DisplayName="Form ID" Description="Unique Form Identificator" Group="Medline Web Portal" SourceID="{cbfbae2b-8362-4201-ac65-6a64a7d1d411}" StaticName="mwp_FormID" AllowDeletion="TRUE" Version="1" Customization="" ColName="nvarchar4" RowOrdinal="0"><Default>0</Default></Field>',
+			"mwp_FormType": '<Field ID="{ac53b379-6156-4aa5-bc14-8fa6136be526}" Type="Text" Name="mwp_FormType" Required="FALSE" DisplayName="Form Type" Description="Give SPA Form Name" Group="Medline Web Portal" SourceID="{cbfbae2b-8362-4201-ac65-6a64a7d1d411}" StaticName="mwp_FormType" AllowDeletion="TRUE" Version="1" Customization="" ColName="nvarchar3" RowOrdinal="0"><Default>Generic SPA Form</Default></Field>',
+			"mwp_FormState": '<Field ID="{2223b379-4a56-25b5-bc14-8f3c5360e526}" Type="Text" Name="mwp_FormState" Required="FALSE" DisplayName="Form State" Description="Form State" Group="Medline Web Portal" SourceID="{cbfbae2b-8362-4201-ac65-6a64a7d1d411}" StaticName="mwp_FormState" AllowDeletion="TRUE" Version="1" Customization="" ColName="nvarchar5" RowOrdinal="0"><Default>Draft</Default></Field>'
 		}
 	};
 
